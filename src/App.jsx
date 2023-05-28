fa6c84437648bcab167cfaa9df6e1fa893a5d7d2
fatiha-dev <tihaelbou@gmail.com>
2023-05-28 23:28:43 +0100

    update preview of cv

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import './App.css';
 import { Template } from './components/ResumeTemplate';
 import { Personalinfos } from './components/Personalinfos';
 import { Education } from './components/Education';
+import { Experience } from './components/Experience';
+import { Skills } from './components/Skills';
 import React from 'react';
 export default function App(){
   const [infos, setInfos] = React.useState({})
   const [education, setEducation] = React.useState({})
+  const [experience, setExperience] = React.useState({})
+  const [skills, setSkills] = React.useState([]);
+  const [skill, setSkill] = React.useState('');
+
 function handleInfos(e){
     const {name, value} = e.target
     return(
@@ -18,7 +24,7 @@ function handleInfos(e){
     )
 }
 function handleEducation(e){
-  const {name, value, type} = e.target
+  const {name, value} = e.target
   return(
     setEducation( previous => {
       return {
@@ -28,6 +34,26 @@ function handleEducation(e){
     })
   )
 }
+function handleEexperience(e){
+  const {name, value, type} = e.target
+  return(
+    setExperience( previous => {
+      return {
+        ...previous,
+        [name]: type === 'checked' ? checked :value,
+      }
+    })
+  )
+}
+function onAddSkill(){
+  if (skill.trim() !== '') {
+    setSkills((prevItems) => [...prevItems, skill]);
+    setSkill('');
+  }
+}
+function handleSkillChange(e){
+  setSkill(e.target.value);
+}
 function handleSubmit(e){
   e.preventDefault()
 }
@@ -36,8 +62,13 @@ return(
     <form className="form flex" onSubmit={handleSubmit}>
          <Personalinfos onChange={handleInfos} />
          <Education onChange={handleEducation} />
+         <Experience onChange={handleEexperience} />
+         <Skills skill={skill} onChange={handleSkillChange} />
     </form>
-    <Template infos={infos} education={education}/>
+    <Template infos={infos}
+              education={education} 
+              experience={experience}
+              skills={skills}/>
   </div>
 )
 }
--- src/components/ResumeTemplate.jsx
+++ src/components/ResumeTemplate.jsx
@@ -1,14 +1,15 @@
-import React from "react"
-export function Template({infos,education}){
+export function Template({infos,education,experience, skills}){
+    
     return(
         <div className="template">
             <div className="cv-section cv-header flex">
             <span className="FullName">
                 {infos.firstName} {infos.lastName}
             </span>
-            <spnan className="Job">{infos.job}</spnan>
+            <span className="Job">{infos.job}</span>
             </div>
             <div className="cv-section">
+                <h3 className="section--header">Contact</h3>
                 <ul className="flex Contact" >
                     <li> {infos.adress} </li>
                     <li> {infos.email} </li>
@@ -16,11 +17,38 @@ export function Template({infos,education}){
                     <li> {infos.portfolio} </li>
                 </ul> 
             </div>
-            <div className="cv-section Education">
-                <p> {education.degree} </p>
-                <p> {education.university} </p>
-                <p> {education.startDate} </p>
-                <p> {education.endDate} </p>
+            <div className="cv-section Education flex">
+                <div>
+                    <h3 className="section--header">Education</h3>
+                </div>
+                <div>
+                    <p className="sec--uni"> {education.university} </p>
+                    <p className="sec--degree"> {education.degree} </p>
+                    <p className="sec--date"> from: {education.startDate} </p>
+                    <p className="sec--date">to:  {education.endDate} </p>  
+                </div>
+                </div>
+            <div className="cv-section Experience flex">
+                <div>
+                    <h3 className="section--header">Experience</h3>
+                </div>
+                <div>
+                <p>{experience.post}
+                    <p className="sec--employer"> {experience.employer} </p>
+                    <p className="sec--date"> from: {experience.workStartAt} </p>
+                    <p className="sec--date"> to: {experience.workEndAt} </p>
+                </p>
+                </div>
+                <div>
+                <p className="post-descripttion"> {experience.postDescription} </p>
+                </div>  
+            </div>
+            <div className="cv-section">
+                <ul>
+                    {skills.map(item => (
+                    <li >{item}</li>
+                ))}
+                </ul>
             </div>
         </div>
     )
