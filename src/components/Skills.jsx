941594763942b6517daf63c79bf1a168c4a01a9f
fatiha-dev <tihaelbou@gmail.com>
2023-05-30 22:29:48 +0100

    edit button style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,17 @@
-export function Skills(){
+export function Skills({onChange, onAddSkill}){
+    
     return(
+        
         <div className="section skills">
-            <h3>Skills</h3>
+            <h3 className="headers">Skills</h3>
             <input className="form-input"
+                    type="text"
                     placeholder="add skills"
+                    onChange={onChange}
             />
+            <div>
+            <button className="icon" onClick={onAddSkill}>Add skill</button>
+            </div>
         </div>
     )
 }
